01a5cd469141cfad9187b4da6b9797c4012a3556
G0blinC0de <brewbrotherred@gmail.com>
2023-12-17 09:31:32 -0800

    add Chad's suggestion for tests

--- __Tests__/nameInput.test.js
+++ __Tests__/nameInput.test.js
@@ -2,7 +2,7 @@ import Player from './../src/nameInput.js';
 
 
 describe(Player, () => {
-    test("Player should exist", () =>{
+    test("Player should exist", () => {
         expect(Player(nameInput, playerClass));
     })
 })
@@ -21,6 +21,34 @@ describe('nameInput', () => {
         expect(player1.playerClass).toBe("Warrior");
     });
 })
+
+
+// ------------- References & Examples ----------------
+
+// import Player from './../src/nameInput.js';
+
+// describe(Player, () => {
+//     test("Player should be created with given name and class", () => {
+//         const player = new Player("Greg", "Warrior");
+//         expect(player).toBeDefined();
+//     });
+// });
+
+// describe('Player properties', () => {
+//     const player1 = new Player("Greg", "Warrior");
+
+//     test("nameInput should be defined", () => {
+//         expect(player1.nameInput).toBeDefined();
+//     });
+
+//     test("nameInput should match the given name", () => {
+//         expect(player1.nameInput).toBe("Greg");
+//     });
+
+//     test('playerClass should match the given class', () => {
+//         expect(player1.playerClass).toBe("Warrior");
+//     });
+// });
 
 
 
